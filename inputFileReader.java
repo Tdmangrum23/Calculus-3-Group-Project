59601875645a2506b2ed74c6ac480d20143aed09
MSUfan918 <tdmangrum23@yahoo.com>
2015-03-27 18:32:44 -0400

    FileReader Test and a.dat file

--- inputFileReader.java
+++ inputFileReader.java
@@ -1,6 +1,23 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import Jama.Matrix;
+
 public class inputFileReader {
 	
-	public Matrix fileReader(File inputFile) {
-	
+	/*public Matrix readFile() throws IOException {
+		String [] args;
+		BufferedReader in = new BufferedReader(new FileReader(args[0]));
+		Matrix mat = Matrix.read(in);
+		System.out.println(mat);
+		return mat;
+	}*/
+
+	public static void main (String[] args) throws IOException {
+		//readFile();
+		BufferedReader in = new BufferedReader(new FileReader(args[0]));
+		Matrix mat = Matrix.read(in);
+		System.out.println(mat);
 	}
 }
