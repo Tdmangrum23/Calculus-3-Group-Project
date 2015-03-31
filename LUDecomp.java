c2cda2c7c5e55cd9ded8e93e5fc3aee58bfaf6ca
emorain3 <emorain@live.com>
2015-03-31 06:06:12 -0400

    LU almost Perfect

--- LUDecomp.java
+++ LUDecomp.java
@@ -1,21 +1,25 @@
 import Jama.Matrix;
 
+import java.util.ArrayList;
+
 public class LUDecomp extends Operations {
 
 
     public static Matrix lu_fact(double[][] A) {
-        Matrix matrix = stabilize(A);
+        Matrix U = stabilize(A);
+        ArrayList<Matrix> eMatrices = new ArrayList<>();
         int rowLength = A.length;
         int colLength = A[0].length;
         int reducingRowIndex = 0;
         for (int col = 0; col <= colLength; col++) {
-            for(int row = col + 1; row < rowLength; row++) {
+            for (int row = col + 1; row < rowLength; row++) {
+                Matrix elementaryMatrix = Matrix.identity(rowLength, colLength);
                 reducingRowIndex = col;
-                double entry = matrix.get(reducingRowIndex, col);
+                double entry = U.get(reducingRowIndex, col);
                 //System.out.println("Entry  " + entry + "  Row  " + (reducingRowIndex) + "  Col  " + (col));
 
                 //use row corresponding to reduce other rows for given column
-                double nextColEntry = matrix.get(row, col);
+                double nextColEntry = U.get(row, col);
                 if (nextColEntry != 0) {
 
                     double reducingFactor = nextColEntry / entry;
@@ -24,14 +28,27 @@ public class LUDecomp extends Operations {
                     double[] reducingVector = Operations.vectorMultiplyByScalar(Operations.getRow(A, reducingRowIndex + 1), reducingFactor);
                     double[] currentRow = Operations.getRow(A, row + 1);
                     double[] newRow = Operations.vectorSubtraction(currentRow, reducingVector);
-                    Operations.setRow(matrix, row + 1, newRow);
-                    matrix.print(5, 3);
+                    Operations.setRow(U, row + 1, newRow);
+                    //U.print(5, 3);
+                    double[] reducingIVector = Operations.vectorMultiplyByScalar(Operations.getRow(elementaryMatrix.getArray(), reducingRowIndex + 1), reducingFactor);
+                    double[] currentIRow = Operations.getRow(elementaryMatrix.getArray(), row + 1);
+                    double[] newIRow = Operations.vectorSubtraction(currentRow, reducingVector);
+                    Operations.setRow(elementaryMatrix, row + 1, newRow);
+                    System.out.println("element Matrix: ");
+                    elementaryMatrix.print(5, 3);
+                    eMatrices.add(elementaryMatrix);
                 }
             }
         }
-
-
-        return matrix;
+        Matrix L = Operations.transpose((eMatrices.get(0).getArray()));
+        for (int index = 1; index < eMatrices.size(); index++) {
+            L = Operations.multiplication(L.getArray(), eMatrices.get(index).getArray());
+        }
+        System.out.println("L is: ");
+        L.print(5, 5);
+        System.out.println("And U is ");
+        U.print(5, 5);
+        return U;
     }
 
 
@@ -61,7 +78,7 @@ public class LUDecomp extends Operations {
 
             }
         }
-        matrix.print(5, 3);
+        matrix.print(5, 5);
         return matrix;
     }
 
